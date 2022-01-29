a30f8bd5ebdab1e3443de968162b1eaebd03fe5d
Litemage <jsimeone0105@gmail.com>
2022-01-29 10:58:26 -0600

    Added angle getter & setters
    - optimized the drive subsystem's handling of value checking, setting, and getting of the home saves

--- src/main/cpp/utils/network_tables_wrapper.cpp
+++ src/main/cpp/utils/network_tables_wrapper.cpp
@@ -4,6 +4,20 @@
 
 #include "utils/network_tables_wrapper.h"
 
+// HELPER FUNCTIONS
+void NetworkTablesWrapper::SetEntryDegrees(nt::NetworkTableEntry& entry, const units::degree_t degrees) {
+  entry.SetDouble(degrees.to<double>());
+}
+
+std::optional<units::degree_t> NetworkTablesWrapper::GetEntryDegrees(nt::NetworkTableEntry& entry) {
+  double savedAngle = entry.GetDouble(NAN);
+  if (savedAngle == NAN) {
+    return std::nullopt;
+  } else {
+    return std::optional<units::degree_t>{savedAngle};
+  }
+}
+
 void NetworkTablesWrapper::InitSwerveTable() {
   m_flHome = m_swerveHomes->GetEntry(networkTables::swerveHomes::keys::flHome);
   m_flHome.SetPersistent();
