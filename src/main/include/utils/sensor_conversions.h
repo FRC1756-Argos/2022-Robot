8998fbc3f72815998b43b13c063ea22c1bf1a66d
wpiformat <dkt01@users.noreply.github.com>
2022-01-29 19:49:40 +0000

    Automated formatting changes

--- src/main/include/utils/sensor_conversions.h
+++ src/main/include/utils/sensor_conversions.h
@@ -1,13 +1,18 @@
+/// \copyright Copyright (c) Argos FRC Team 1756.
+///            Open Source Software; you can modify and/or share it under the terms of
+///            the license file in the root directory of this project.
+
 #pragma once
 #include "units/Angle.h"
-namespace sensor_conversions{
-  namespace turret{
-    constexpr double sensorConversionFactor=4096.0/360; ///< sensor units per degree
-   constexpr double ToSensorUnit(const units::degree_t degrees){
-     return sensorConversionFactor*degrees.to<double>();
-   }
-   constexpr units::degree_t ToAngle (const double sensorunit){
-     return units::make_unit<units::degree_t>(sensorunit/sensorConversionFactor);
-   }
-  }
-}
+
+namespace sensor_conversions {
+  namespace turret {
+    constexpr double sensorConversionFactor = 4096.0 / 360;  ///< sensor units per degree
+    constexpr double ToSensorUnit(const units::degree_t degrees) {
+      return sensorConversionFactor * degrees.to<double>();
+    }
+    constexpr units::degree_t ToAngle(const double sensorunit) {
+      return units::make_unit<units::degree_t>(sensorunit / sensorConversionFactor);
+    }
+  }  // namespace turret
+}  // namespace sensor_conversions
