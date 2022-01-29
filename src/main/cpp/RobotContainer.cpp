667b68b746edc2a51f444aaba5f682154320e088
ErnestSally <25sallye@limestone310.org>
2022-01-28 20:06:17 -0600

    we added manual shooting controlls

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -5,11 +5,13 @@
 #include "RobotContainer.h"
 
 #include <frc2/command/RunCommand.h>
-
+#include <frc2/command/button/Trigger.h>
 RobotContainer::RobotContainer()
     : m_driveLonSpeedMap(controllerMap::driveLongSpeed)
     , m_driveLatSpeedMap(controllerMap::driveLatSpeed)
     , m_driveRotSpeed(controllerMap::driveRotSpeed)
+    , m_turretSpeedMap(controllerMap::turretSpeed)
+    , m_hoodSpeedMap(controllerMap::hoodSpeed)
     , m_controllers(address::controllers::driver, address::controllers::secondary)
     , m_swerveDrive()
     , m_shooter() {
@@ -26,17 +28,27 @@ RobotContainer::RobotContainer()
       {&m_swerveDrive}));
   m_shooter.SetDefaultCommand(frc2::RunCommand(
       [this] {
-        m_shooter.ManualAim(m_driveLonSpeedMap(m_controllers.OperatorController().GetX(
+        m_shooter.ManualAim(m_turretSpeedMap(m_controllers.OperatorController().GetX(
                                 argos_lib::XboxController::JoystickHand::kLeftHand)),
-                            m_driveLatSpeedMap(m_controllers.OperatorController().GetY(
+                            m_hoodSpeedMap(m_controllers.OperatorController().GetY(
                                 argos_lib::XboxController::JoystickHand::kLeftHand)));
       },
       {&m_shooter}));
+
   ConfigureButtonBindings();
 }
 
 void RobotContainer::ConfigureButtonBindings() {
   // Configure your button bindings here
+  auto shooter = (frc2::Trigger{[this]() {
+    return m_controllers.DriverController().GetRawButton(argos_lib::XboxController::Button::kLeftTrigger);
+  }});
+shooter.WhenActive([this](){
+  m_shooter.shooting(1);
+}, {&m_shooter});
+shooter.WhenInactive([this](){
+  m_shooter.shooting(0);
+}, {&m_shooter});
 }
 
 frc2::Command* RobotContainer::GetAutonomousCommand() {
--- src/main/include/RobotContainer.h
+++ src/main/include/RobotContainer.h
@@ -36,6 +36,14 @@ class RobotContainer {
 
   argos_lib::InterpolationMap<decltype(controllerMap::driveRotSpeed.front().inVal), controllerMap::driveRotSpeed.size()>
       m_driveRotSpeed;
+
+     argos_lib::InterpolationMap<decltype(controllerMap::turretSpeed.front().inVal),
+                              controllerMap::turretSpeed.size()>
+      m_turretSpeedMap;
+  argos_lib::InterpolationMap<decltype(controllerMap::hoodSpeed.front().inVal),
+                              controllerMap::hoodSpeed.size()>
+      m_hoodSpeedMap;
+
   argos_lib::SwappableControllersSubsystem m_controllers;
   SwerveDriveSubsystem m_swerveDrive;
   ShooterSubsystem m_shooter;
