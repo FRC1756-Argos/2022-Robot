de7a213293c73ac729450c293f426fde97c81e5e
ErnestSally <25sallye@limestone310.org>
2022-01-31 20:52:38 -0600

    added hood namespace

--- src/main/include/utils/sensor_conversions.h
+++ src/main/include/utils/sensor_conversions.h
@@ -15,4 +15,13 @@ namespace sensor_conversions {
       return units::make_unit<units::degree_t>(sensorunit / sensorConversionFactor);
     }
   }  // namespace turret
+  namespace hood {
+    constexpr double sensorConversionFactor = 4096.0 / 360;  ///< sensor units per degree
+    constexpr double ToSensorUnit(const units::degree_t degrees) {
+      return sensorConversionFactor * degrees.to<double>();
+    }
+    constexpr units::degree_t ToAngle(const double sensorunit) {
+      return units::make_unit<units::degree_t>(sensorunit / sensorConversionFactor);
+    }
+  }  // namespace hood
 }  // namespace sensor_conversions
