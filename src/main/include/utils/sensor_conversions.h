5e42da579af02f1498681f758dcf2f45d0af5d47
ErnestSally <25sallye@limestone310.org>
2022-02-01 20:23:32 -0600

    made the unit conversion changes for the hood

--- src/main/include/utils/sensor_conversions.h
+++ src/main/include/utils/sensor_conversions.h
@@ -16,12 +16,17 @@ namespace sensor_conversions {
     }
   }  // namespace turret
   namespace hood {
-    constexpr double sensorConversionFactor = 4096.0 / 360;  ///< sensor units per degree
+    constexpr double sensorConversionFactor = 360.0 / 4096;  ///< sensor units per degree
+    constexpr double sensorConversionDegTeeth = 16.0 / 360;
+    constexpr double sensorConversionTeethIn = 4.0 / 16;
+    constexpr double sensorConversionInAngle = 2 * M_PI / (360 * 11.5);
     constexpr double ToSensorUnit(const units::degree_t degrees) {
-      return sensorConversionFactor * degrees.to<double>();
+      return degrees.to<double>() / sensorConversionInAngle / sensorConversionTeethIn / sensorConversionDegTeeth /
+             sensorConversionFactor;
     }
     constexpr units::degree_t ToAngle(const double sensorunit) {
-      return units::make_unit<units::degree_t>(sensorunit / sensorConversionFactor);
+      return units::make_unit<units::degree_t>(sensorunit * sensorConversionFactor * sensorConversionDegTeeth *
+                                               sensorConversionTeethIn * sensorConversionInAngle);
     }
   }  // namespace hood
 }  // namespace sensor_conversions
