13c890726d9a5d4ecd8fa99f0cc2b22231398dca
David K Turner <dkt01@users.noreply.github.com>
2022-02-02 15:32:42 -0600

    Add more detailed comments for conversion stages

--- src/main/include/utils/sensor_conversions.h
+++ src/main/include/utils/sensor_conversions.h
@@ -7,7 +7,7 @@
 
 namespace sensor_conversions {
   namespace turret {
-    constexpr double sensorConversionFactor = 4096.0 / 360;  ///< sensor units per degree
+    constexpr double sensorConversionFactor = 4096.0 / 360;  ///< multiply to convert turret rotation degrees to raw sensor units
     constexpr double ToSensorUnit(const units::degree_t degrees) {
       return sensorConversionFactor * degrees.to<double>();
     }
@@ -16,10 +16,10 @@ namespace sensor_conversions {
     }
   }  // namespace turret
   namespace hood {
-    constexpr double sensorConversionFactor = 360.0 / 4096;  ///< sensor units per degree
-    constexpr double sensorConversionDegTeeth = 16.0 / 360;
-    constexpr double sensorConversionTeethIn = 4.0 / 16;
-    constexpr double sensorConversionInAngle = 2 * M_PI / (360 * 11.5);
+    constexpr double sensorConversionFactor = 360.0 / 4096;  ///< multiply to convert raw sensor units to motor output shaft degrees
+    constexpr double sensorConversionDegTeeth = 16.0 / 360;  ///< multiply to convert motor output shaft degrees to hood sproket teeth
+    constexpr double sensorConversionTeethIn = 4.0 / 16;     ///< multiply to convert hood sproket teeth to hood extension distance
+    constexpr double sensorConversionInAngle = 2 * M_PI / (360 * 11.5); ///< multiply to convert hood extension distance to hood angle
     constexpr double ToSensorUnit(const units::degree_t degrees) {
       return degrees.to<double>() / sensorConversionInAngle / sensorConversionTeethIn / sensorConversionDegTeeth /
              sensorConversionFactor;
