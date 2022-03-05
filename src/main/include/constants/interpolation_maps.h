4be0a6c3367a33e06ab2cb6d404ff221e9edeb48
wpiformat <41898282+github-actions[bot]@users.noreply.github.com>
2022-03-05 04:09:34 +0000

    Automated formatting changes

--- src/main/include/constants/interpolation_maps.h
+++ src/main/include/constants/interpolation_maps.h
@@ -14,20 +14,18 @@
 
 namespace shooterRange {
   using argos_lib::InterpMapPoint;
-  [[maybe_unused]] constexpr std::array shooterSpeed{
-      InterpMapPoint{10.0_in, 2300.0_rpm},
-      InterpMapPoint{21.5_in, 2350.0_rpm},
-      InterpMapPoint{60.0_in, 2650.0_rpm},
-      InterpMapPoint{120.0_in, 2975.0_rpm},
-      InterpMapPoint{180.0_in, 3200.0_rpm},
-      InterpMapPoint{240.0_in, 3425.0_rpm}};
-  [[maybe_unused]] constexpr std::array hoodAngle{
-      InterpMapPoint{10.0_in, 10.0_deg},
-      InterpMapPoint{21.5_in, 15.0_deg},
-      InterpMapPoint{60.0_in, 25.0_deg},
-      InterpMapPoint{120.0_in, 30.0_deg},
-      InterpMapPoint{180.0_in, 37.0_deg},
-      InterpMapPoint{240.0_in, 42.0_deg}};
+  [[maybe_unused]] constexpr std::array shooterSpeed{InterpMapPoint{10.0_in, 2300.0_rpm},
+                                                     InterpMapPoint{21.5_in, 2350.0_rpm},
+                                                     InterpMapPoint{60.0_in, 2650.0_rpm},
+                                                     InterpMapPoint{120.0_in, 2975.0_rpm},
+                                                     InterpMapPoint{180.0_in, 3200.0_rpm},
+                                                     InterpMapPoint{240.0_in, 3425.0_rpm}};
+  [[maybe_unused]] constexpr std::array hoodAngle{InterpMapPoint{10.0_in, 10.0_deg},
+                                                  InterpMapPoint{21.5_in, 15.0_deg},
+                                                  InterpMapPoint{60.0_in, 25.0_deg},
+                                                  InterpMapPoint{120.0_in, 30.0_deg},
+                                                  InterpMapPoint{180.0_in, 37.0_deg},
+                                                  InterpMapPoint{240.0_in, 42.0_deg}};
 }  // namespace shooterRange
 namespace controllerMap {
   using argos_lib::InterpMapPoint;
