ad1fa60099818b4e6c9a6d2f3dc29edbd7e59e1d
Nikdogge <npadarthy@gmail.com>
2022-03-11 20:30:46 -0600

    Fine tuned shooter aiming function

--- src/main/cpp/subsystems/shooter_subsystem.cpp
+++ src/main/cpp/subsystems/shooter_subsystem.cpp
@@ -126,10 +126,10 @@ void ShooterSubsystem::AutoAim() {
     distanceToTarget = GetTargetDistance(targetValues.pitch);
   }
 
-  if (distanceToTarget > (units::inch_t)140) {
-    // fitting 2nd degree polynomial to get the offset
-    distanceToTarget -= GetPolynomialOffset(distanceToTarget);
-  }
+  frc::SmartDashboard::PutNumber("(Auto-Aim) Target distance without offset", distanceToTarget.to<double>());
+
+  // fitting 2nd degree polynomial to get the offset
+  distanceToTarget -= GetPolynomialOffset(distanceToTarget);
 
   const auto shooterSetpoints = SetShooterDistance(distanceToTarget);
 
@@ -148,9 +148,18 @@ void ShooterSubsystem::AutoAim() {
 
 units::inch_t ShooterSubsystem::GetPolynomialOffset(units::inch_t actualDistance) {
   units::inch_t offset;
-  double y = (45 - (0.4166667 * actualDistance.to<double>()) +
-              (0.001388889 * (actualDistance.to<double>() * actualDistance.to<double>())));
-  offset = (units::inch_t)y;
+  if (actualDistance >= (units::inch_t)160) {
+    double y =
+        (50 - (0.4166667 * actualDistance.to<double>()) + (0.001388889 * std::pow(actualDistance.to<double>(), 2)));
+    offset = (units::inch_t)y;
+
+  } else if (actualDistance >= (units::inch_t)90 && actualDistance < (units::inch_t)160) {
+    double y = (2.928571 - (0.1528571 * actualDistance.to<double>()) +
+                (0.001428571 * std::pow(actualDistance.to<double>(), 2)));
+    offset = (units::inch_t)y;
+  } else {
+    offset = (units::inch_t)0;
+  }
   return offset;
 }
 
