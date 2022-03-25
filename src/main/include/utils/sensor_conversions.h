2ecd69e7c3a5b9ae034bb5c241c40758d271437e
dkt01 <d.k.turner@att.net>
2022-03-25 14:52:17 -0500

    Change to 12:1 versa max gear box for hooks

--- src/main/include/utils/sensor_conversions.h
+++ src/main/include/utils/sensor_conversions.h
@@ -126,17 +126,18 @@ namespace sensor_conversions {
   }  // namespace climb_arms
   namespace climb_hooks {
     constexpr double sensorToMotorRev = 1.0 / 2048;
-    constexpr double gearboxReduction = 1.0 / 8;
+    constexpr double gearboxReduction = 1.0 / 12;
     constexpr double driveSprocketTeethPerRevolution = 18.0;
     constexpr double extensionInchesPerDriveSprocketTooth = 0.25 / 1;
-    constexpr double fudgeFactor = 1;  ///< @todo Why are we off by this amount? // <--- Because the teeth weren't right
+    constexpr double fudgeFactor = 1.0;
     constexpr units::inch_t ToExtension(const double sensorUnit) {
       return units::make_unit<units::inch_t>(sensorUnit * sensorToMotorRev * gearboxReduction *
-                                             driveSprocketTeethPerRevolution * extensionInchesPerDriveSprocketTooth);
+                                             driveSprocketTeethPerRevolution * extensionInchesPerDriveSprocketTooth *
+                                             fudgeFactor);
     }
     constexpr double ToSensorUnit(const units::inch_t extension) {
-      return extension.to<double>() / extensionInchesPerDriveSprocketTooth / driveSprocketTeethPerRevolution /
-             gearboxReduction / sensorToMotorRev;
+      return extension.to<double>() / fudgeFactor / extensionInchesPerDriveSprocketTooth /
+             driveSprocketTeethPerRevolution / gearboxReduction / sensorToMotorRev;
     }
 
     constexpr double ToSensorVelocity(const units::meters_per_second_t velocity) {
