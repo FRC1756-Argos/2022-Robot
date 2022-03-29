99f7bfd9e1c2d6267c412d3fad6226aca98f0655
David K Turner <dkt01@users.noreply.github.com>
2022-03-29 15:27:20 -0500

    Reverting reversion of shooting fudge factor
    Apparently this is making competition robot shot more consistent at practice field

--- src/main/cpp/subsystems/shooter_subsystem.cpp
+++ src/main/cpp/subsystems/shooter_subsystem.cpp
@@ -118,7 +118,7 @@ bool ShooterSubsystem::AutoAim(bool drivingAdjustment) {
   // Get target distance & assign to hood & shooter
   units::length::inch_t distanceToTarget;
 
-  units::length::inch_t fudgeFactor = 0_in;  // 12_in at CIR
+  units::length::inch_t fudgeFactor = 12_in;  // 12_in at CIR and now comp bot on practice field?
 
   if (m_useCalculatedPitch) {
     units::degree_t newPitch = m_cameraInterface.GetNewPitch(
