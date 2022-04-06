85db4f32da3844de4d03c90da2f902b96a0fb718
dkt01 <d.k.turner@att.net>
2022-04-05 21:11:28 -0500

    Driving shot kind of works except for lens/perspective distortion messing up distance calculation

--- src/main/include/constants/measure_up.h
+++ src/main/include/constants/measure_up.h
@@ -7,6 +7,10 @@
 #include <units/angle.h>
 #include <units/length.h>
 
+#include <array>
+
+#include <opencv2/calib3d.hpp>
+
 namespace measure_up {
   constexpr auto intakeExtension = 18_cm;  ///< Distance from frame to center of intake in x direction
   constexpr auto bumperExtension = 3_in;   ///< Distance from frame to outer edge of bumpers
@@ -43,6 +47,24 @@ namespace measure_up {
     constexpr auto cameraMountAngle = 27_deg;               // actual current camera mount angle is 27.8 degrees
     constexpr auto cameraMountAnglePracticeBot = 28.3_deg;  // actual currect camera mount angle is 29.1
     constexpr auto toRotationCenter = 9_in;
+
+    // const float cameraIntrinsicValues[] =
+    static const cv::Matx33d intrinsics{2.5751292067328632e+02,
+                                        0.,
+                                        1.5971077914723165e+02,
+                                        0.,
+                                        2.5635071715912881e+02,
+                                        1.1971433393615548e+02,
+                                        0.,
+                                        0.,
+                                        1.};
+    static const cv::Matx<double, 1, 5> distortion{2.9684613693070039e-01,
+                                                   -1.4380252254747885e+00,
+                                                   -2.2098421479494509e-03,
+                                                   -3.3894563533907176e-03,
+                                                   2.5344430354806740e+00};
+    // const cv::Mat1f cameraIntrinsics(3, 3, &cameraIntrinsicValues[0]);
+
   }  // namespace camera
   namespace climber_arm {
     constexpr auto homeExtension = 21.5_in;  ///< Length between mount points
