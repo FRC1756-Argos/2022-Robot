5434620de16da62fd40937ed3f89486fbc4978e1
Kanakam Teja Maddala <kanakamteja@gmail.com>
2022-04-06 15:25:58 -0500

    send pixel loc and angles to the top-center of the target and initial perspective transform

--- vision/limelight/customArgos.py
+++ vision/limelight/customArgos.py
@@ -14,6 +14,33 @@ cameraMatrix = np.array([[772.53876202/3.0, 0, 479.132337442/3.0], [0, 769.05215
 distortionCoeff = np.array([[2.9684613693070039e-01, -1.4380252254747885e+00,-2.2098421479494509e-03,
                         -3.3894563533907176e-03, 2.5344430354806740e+00]])
 
+distortedPts1 = np.float32([[-30, -30], [160, 0], [0, 240], [160, 240]])
+desiredPts1 = np.float32([[0, 0], [160, 0], [0, 240], [160, 240]])
+distortedPts2 = np.float32([[160, 0], [350, 30], [320, 240], [160, 240]])
+desiredPts2 = np.float32([[160, 0], [320, 0], [320, 240], [160, 240]])
+
+
+def getTargetAtTop(box):
+    blx = box[0][0]
+    bly = box[0][1]
+    brx = box[1][0]
+    bry = box[1][1]
+    
+    px = (blx + brx)/2.0
+    py = (bly + bry)/2.0
+    nx = (1/160) * (px - 159.5)
+    ny = (1/120) * (119.5 - py)
+
+    vpw = 2.0*np.tan(H_FOV/2)
+    vph = 2.0*np.tan(V_FOV/2)
+
+    nx = vpw/2 * nx;
+    ny = vph/2 * ny;
+
+    tx = np.atan2(1, nx)
+    ty = np.atan2(1, ny)
+    
+    return px, py, tx, ty
 # To change a global variable inside a function,
 # re-declare it the global keyword
 def debugData(box):
@@ -38,12 +65,18 @@ def drawDecorations(image):
 def runPipeline(image, llrobot):
 
     dst = image
+    
+    #Apply Perspective Transforms
+    matrix1 = cv2.getPerspectiveTransform(distortedPts1, desiredPts1)
+    matrix2 = cv2.getPerspectiveTransform(distortedPts1, desiredPts1)
+    matrix = matrix1 * matrix2
+    undistorted = cv2.warpPerspective(dst, matrix, (320, 240))
 
-    hd,  wd = image.shape[:2]
-    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(cameraMatrix, distortionCoeff, (wd,hd), 1, (wd,hd))
+    #hd,  wd = image.shape[:2]
+    #newcameramtx, roi = cv2.getOptimalNewCameraMatrix(cameraMatrix, distortionCoeff, (wd,hd), 1, (wd,hd))
 
     # undistort
-    dst = cv2.undistort(image, cameraMatrix, distortionCoeff, None, newcameramtx)
+    #dst = cv2.undistort(image, cameraMatrix, distortionCoeff, None, newcameramtx)
 
     # method-2
     #mapx,mapy=cv2.initUndistortRectifyMap(cameraMatrix,distortionCoeff,None,newcameramtx,(wd,hd),5)
@@ -64,6 +97,7 @@ def runPipeline(image, llrobot):
     fittedRedBox = np.array([[]])
 
     llpython = [0,0,0,0,0,0,0,0]
+    px, py, tx, ty, isValid = 0, 0, 0, 0, 0
 
     if (len(contours) > 0 and len(contours) < 7):
         filteredCountours = []
@@ -86,12 +120,16 @@ def runPipeline(image, llrobot):
         x,y,w,h = cv2.boundingRect(groupedSmartContour)
 
         cv2.rectangle(dst,(x,y),(x+w,y+h),(0,255,255),2)
-        llpython = [1,x,y,w,h,9,8,7]
         rect = cv2.minAreaRect(groupedSmartContour)
         fittedRedBox = cv2.boxPoints(rect)
         fittedRedBox = np.int0(fittedRedBox)
         cv2.drawContours(dst,[fittedRedBox],0,(0,0,255),2)
-
+        
+        #send the position of the top of the hub along with center
+        if fittedRedBox.size:
+          isValid = 1
+          px, py, tx, ty = getTargetAtTop(fittedRedBox)
+        llpython = [isValid,px,py,tx,ty,9,8,7]
 
     debugData(fittedRedBox)
     drawDecorations(dst)
@@ -100,4 +138,4 @@ def runPipeline(image, llrobot):
     # an image to stream,
     # and optionally an array of up to 8 values for the "llpython"
     # networktables array
-    return groupedSmartContour, dst, llpython
+    return groupedSmartContour, undistorted, llpython
