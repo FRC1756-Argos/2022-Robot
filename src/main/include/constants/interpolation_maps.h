3d091cb5f15f3cb5c5e3a738600e39e1aa5ffe31
dkt01 <d.k.turner@att.net>
2022-04-14 19:20:49 -0500

    Disable old hub shot setpoints to hopefully improve <5' shot consistency

--- src/main/include/constants/interpolation_maps.h
+++ src/main/include/constants/interpolation_maps.h
@@ -16,7 +16,7 @@ namespace shooterRange {
   using argos_lib::InterpMapPoint;
   [[maybe_unused]] constexpr std::array shooterSpeed{InterpMapPoint{10.0_in, 2550.0_rpm},
                                                      InterpMapPoint{21.5_in, 2500.0_rpm},
-                                                     InterpMapPoint{21.6_in, 2350.0_rpm},
+                                                     //  InterpMapPoint{21.6_in, 2350.0_rpm},
                                                      InterpMapPoint{60.0_in, 2500.0_rpm},
                                                      InterpMapPoint{120.0_in, 2750.0_rpm},
                                                      InterpMapPoint{180.0_in, 3150.0_rpm},
@@ -25,7 +25,7 @@ namespace shooterRange {
                                                      InterpMapPoint{360.0_in, 4250.0_rpm}};
   [[maybe_unused]] constexpr std::array hoodAngle{InterpMapPoint{10.0_in, 6.5_deg},
                                                   InterpMapPoint{21.5_in, 6.5_deg},
-                                                  InterpMapPoint{21.6_in, 7.0_deg},
+                                                  //   InterpMapPoint{21.6_in, 7.0_deg},
                                                   InterpMapPoint{60.0_in, 13.0_deg},
                                                   InterpMapPoint{120.0_in, 20.0_deg},
                                                   InterpMapPoint{180.0_in, 29.0_deg},
